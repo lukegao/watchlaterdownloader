15a6fc7d74665a9b2d8ef47f3a5f6c8c70a94cf7
lukegao <lukegao@gmail.com>
2015-01-08 18:39:55 +0800

    updates with GTM oauth module

---
+++ MacApp/GTM-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  GTM-Bridging-Header.h
+//  MacApp
+//
+//  Created by Luke Gao on 1/7/15.
+//  Copyright (c) 2015 Luke Gao. All rights reserved.
+//
+
+#ifndef MacApp_GTM_Bridging_Header_h
+#define MacApp_GTM_Bridging_Header_h
+
+#import "GTMOAuth2WindowController.h"
+
+#endif
--- MacApp/ViewController.swift
+++ MacApp/ViewController.swift
@@ -7,13 +7,34 @@
 //
 
 import Cocoa
+import Foundation
+import AppKit
+
+let CLIENT_ID = "365805353701-rq0srr6lhrs65seots6oqf4grl61bi2i.apps.googleusercontent.com"
+let CLIENT_SECRET = "XWKZEDuuC-b52B9p7AOYYiFO"
+let YOUTUBE_SCOPE = "https://www.googleapis.com/auth/youtube"
 
 class ViewController: NSViewController {
+    @IBOutlet weak var userInfoButton: NSButton!
+    
+    var auth: GTMOAuth2Authentication! {
+        didSet {
+            if let mail = auth.userEmail {
+                self.userInfoButton.title = mail
+                self.userInfoButton.transparent = false
+            } else {
+                self.userInfoButton.title = ""
+                self.userInfoButton.transparent = true
+            }
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        self.userInfoButton.transparent = true
+        self.auth = GTMOAuth2WindowController.authForGoogleFromKeychainForName("WatchLaterDownloader", clientID: CLIENT_ID, clientSecret: CLIENT_SECRET)
     }
 
     override var representedObject: AnyObject? {
@@ -21,7 +42,27 @@ class ViewController: NSViewController {
         // Update the view, if already loaded.
         }
     }
-
-
+    
+    @IBAction func showUserInfo(sender: NSButton) {
+    }
+    
+    @IBAction func oauthGTM(sender: NSButton) {
+        var gtmvc = GTMOAuth2WindowController(scope: YOUTUBE_SCOPE, clientID: CLIENT_ID, clientSecret: CLIENT_SECRET, keychainItemName: "WatchLaterDownloader", resourceBundle: nil)
+        
+        gtmvc.signInSheetModalForWindow(self.view.window, completionHandler: {
+            (auth: GTMOAuth2Authentication!, error: NSError!) in
+            if error != nil {
+                println(error)
+            }
+            self.auth = auth
+        })
+    }
+    
+    @IBAction func signOut(sender: NSButton) {
+        GTMOAuth2WindowController.removeAuthFromKeychainForName("WatchLaterDownloader")
+        GTMOAuth2WindowController.revokeTokenForGoogleAuthentication(self.auth)
+        self.userInfoButton.title = ""
+        self.userInfoButton.transparent = true
+    }
+    
 }
-
